e357e05da6b7e38520b82778d0ecde132c955533
Sohoxic <pes2202100825@pesu.pes.edu>
2024-01-27 11:55:49 +0530

    added atoi

---
+++ recursion/atoi.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int myAtoi(string s) {
+        int i=0;
+        int sign = 1;
+        int ans = 0;
+        while(i<s.length() && s[i]==' '){
+            i++;
+        }
+        if(s[i] == '-'){
+            sign = -1;
+            i++;
+        }
+        else if(s[i]=='+')  i++;
+        while(i<s.length()){
+            if(s[i]>='0' && s[i]<='9'){
+                ans = ans*10+(s[i]-'0');
+                if(ans>INT_MAX && sign==-1)
+                    return INT_MIN;
+                else if(ans>INT_MAX && sign==1)
+                    return INT_MAX;
+                i++;
+            }
+            else
+                return ans*sign;
+        }
+        return ans*sign;
+}        
+
+int main(){
+    cout<<myAtoi("  -42");
+}
