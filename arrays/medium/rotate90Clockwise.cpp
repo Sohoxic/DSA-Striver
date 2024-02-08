4743df2978d5ea9d0411df4ce2c1393aa4b437b8
Sohoxic <pes2202100825@pesu.pes.edu>
2024-02-08 14:47:12 +0530

    added optimal code for rotate matrix by 90 clockwise

--- arrays/medium/rotate90Clockwise.cpp
+++ arrays/medium/rotate90Clockwise.cpp
@@ -1,27 +1,62 @@
+// Brute Force
+
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// vector < vector < int >> rotate(vector < vector < int >> & matrix) {
+//     int n = matrix.size();
+//     vector < vector < int >> rotate(n, vector < int > (n, 0));
+//     for (int i = 0; i < n; i++) {
+//         for (int j = 0; j < n; j++) {
+//             rotate[j][n-i-1] = matrix[i][j];
+//         }
+//     }
+
+//     return rotate;
+// }
+
+// int main() {
+//     vector < vector < int >> arr;
+//     arr =  {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+//     vector < vector < int >> rotated = rotate(arr);
+//     cout << "Rotated Image" << endl;
+//     for (int i = 0; i < rotated.size(); i++) {
+//     for (int j = 0; j < rotated[0].size(); j++) {
+//         cout << rotated[i][j] << " ";
+//     }
+//     cout <<endl;
+//     }
+
+// }
+
+// Optimal approach: Transpose and Reverse
+
 
 #include<bits/stdc++.h>
+
 using namespace std;
-
-vector < vector < int >> rotate(vector < vector < int >> & matrix) {
+void rotate(vector < vector < int >> & matrix) {
     int n = matrix.size();
-    vector < vector < int >> rotate(n, vector < int > (n, 0));
+    //transposing the matrix
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            rotate[j][n-i-1] = matrix[i][j];
-        }
+    for (int j = 0; j < i; j++) {
+        swap(matrix[i][j], matrix[j][i]);
     }
-
-    return rotate;
+    }
+    //reversing each row of the matrix
+    for (int i = 0; i < n; i++) {
+    reverse(matrix[i].begin(), matrix[i].end());
+    }
 }
 
 int main() {
     vector < vector < int >> arr;
     arr =  {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-    vector < vector < int >> rotated = rotate(arr);
+    rotate(arr);
     cout << "Rotated Image" << endl;
-    for (int i = 0; i < rotated.size(); i++) {
-    for (int j = 0; j < rotated[0].size(); j++) {
-        cout << rotated[i][j] << " ";
+    for (int i = 0; i < arr.size(); i++) {
+    for (int j = 0; j < arr[0].size(); j++) {
+        cout << arr[i][j] << " ";
     }
     cout <<endl;
     }
