240b0e8bd4a343cea1ffc40166d200075ad78b25
Sohoxic <pes2202100825@pesu.pes.edu>
2024-02-08 10:44:56 +0530

    added better solution of set matrix zero

--- arrays/medium/setMatrixZero.cpp
+++ arrays/medium/setMatrixZero.cpp
@@ -1,56 +1,107 @@
-#include<bits/stdc++.h>
+// Brute Force
+
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// vector<vector<int>> setMatrix(vector<vector<int>> &matrix, int row, int col, int m, int n){
+//     // row
+//     for(int i=0; i<m; i++){
+//         if(matrix[i][col]!=0){
+//             matrix[i][col] = -1;
+//         }
+//     }
+//     // column
+//     for(int j=0; j<n; j++){
+//         if(matrix[row][j]!=0){
+//             matrix[row][j] = -1;
+//         }
+//     }
+
+//     return matrix;
+// }
+
+// vector<vector<int>> zeroMatrix(vector<vector<int>> &matrix, int m, int n){
+//     for(int i=0; i<m; i++){
+//         for(int j=0; j<n; j++){
+//             if(matrix[i][j] == 0){
+//                 matrix = setMatrix(matrix, i, j, m, n);
+//             }
+//         }
+//     }
+
+
+//     for(int i=0; i<m; i++){
+//         for(int j=0; j<n; j++){
+//             if(matrix[i][j] == -1){
+//                 matrix[i][j] = 0;
+//             }
+//         }
+//     }
+
+//     return matrix;
+// }
+// int main()
+// {
+//     vector<vector<int>> matrix = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+//     int m = matrix.size();
+//     int n = matrix[0].size();
+//     vector<vector<int>> ans = zeroMatrix(matrix, m, n);
+
+//     cout << "The Final matrix is:";
+//     for (auto it : ans) {
+//         for (auto ele : it) {
+//             cout << ele << " ";
+//         }
+//         cout <<endl;
+//     }
+//     return 0;
+// }
+
+// Better Approach
+
+
+#include <bits/stdc++.h>
 using namespace std;
 
-vector<vector<int>> setMatrix(vector<vector<int>> &matrix, int row, int col, int m, int n){
-    // row
-    for(int i=0; i<m; i++){
-        if(matrix[i][col]!=0){
-            matrix[i][col] = -1;
-        }
-    }
-    // column
-    for(int j=0; j<n; j++){
-        if(matrix[row][j]!=0){
-            matrix[row][j] = -1;
-        }
-    }
+vector<vector<int>> zeroMatrix(vector<vector<int>> &matrix, int m, int n) {
 
-    return matrix;
-}
+    int row[m] = {0}; // row array
+    int col[n] = {0}; // col array
 
-vector<vector<int>> zeroMatrix(vector<vector<int>> &matrix, int m, int n){
     for(int i=0; i<m; i++){
         for(int j=0; j<n; j++){
             if(matrix[i][j] == 0){
-                matrix = setMatrix(matrix, i, j, m, n);
+                row[i] = 1;
+                col[j] = 1;
             }
         }
-    }
-
+    }   
 
     for(int i=0; i<m; i++){
         for(int j=0; j<n; j++){
-            if(matrix[i][j] == -1){
+            if(row[i] || col[j]){
                 matrix[i][j] = 0;
             }
         }
     }
 
+
     return matrix;
 }
+
 int main()
 {
     vector<vector<int>> matrix = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
     int m = matrix.size();
-    int n = matrix[0].size();
+    int n= matrix[0].size();
     vector<vector<int>> ans = zeroMatrix(matrix, m, n);
 
-    cout << "The Final matrix is:";
+    cout << "The Final matrix is: n";
     for (auto it : ans) {
         for (auto ele : it) {
             cout << ele << " ";
         }
-        cout <<endl;
+        cout << "n";
     }
     return 0;
 }
