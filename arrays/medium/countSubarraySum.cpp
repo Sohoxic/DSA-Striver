c442df7d996f3985f3b93819b73e29c26628a590
Sohoxic <pes2202100825@pesu.pes.edu>
2024-02-09 13:07:32 +0530

    added count subarray sum k

---
+++ arrays/medium/countSubarraySum.cpp
@@ -0,0 +1,30 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int findAllSubarraysWithGivenSum(vector < int > & arr, int k) {
+    int n = arr.size();
+    int cnt = 0; 
+    
+    for(int i=0; i<n; i++){
+        int sum = 0;
+        for(int j=i; j<n; j++){
+            sum+= arr[j];
+            if(sum == k){
+                cnt++;
+            }
+        }
+    }
+
+    return cnt;
+}
+
+int main()
+{
+    vector arr = {3, 1, 2, 4};
+    int k = 6;
+    int cnt = findAllSubarraysWithGivenSum(arr, k);
+    cout << "The number of subarrays is: " << cnt << "\n";
+    return 0;
+}
+
