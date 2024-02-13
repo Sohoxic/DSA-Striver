2562ebf365b17654f81ecc83e881bb75597f9477
Sohoxic <pes2202100825@pesu.pes.edu>
2024-02-13 07:33:49 +0530

    sorted array and modified if else

--- arrays/hard/overlappingSubintervals.cpp
+++ arrays/hard/overlappingSubintervals.cpp
@@ -1,16 +1,15 @@
-
-
-
+// Brute
 #include <bits/stdc++.h>
 using namespace std;
 
 vector<vector<int>> mergeOverlappingIntervals(vector<vector<int>> &arr) {
     int n = arr.size();
+    sort(arr.begin(), arr.end());
     vector<vector<int>> ans;
     for(int i=0; i<n; i++){
         int start = arr[i][0];
         int end = arr[i][1];
-        if(!ans.empty() && ans.back()[1]>end){
+        if(!ans.empty() && ans.back()[1]>=end){
             continue;
         }
         for(int j=i+1; j<n; j++){
@@ -38,5 +37,4 @@ int main()
     cout << endl;
     return 0;
 }
-
 
