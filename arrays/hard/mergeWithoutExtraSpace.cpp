044a30d5c71aa11455a40e7883d8fd931caa443f
Sohoxic <pes2202100825@pesu.pes.edu>
2024-02-13 08:02:32 +0530

    added optimal merge two sorted arrays

--- arrays/hard/mergeWithoutExtraSpace.cpp
+++ arrays/hard/mergeWithoutExtraSpace.cpp
@@ -1,31 +1,71 @@
-// Brute: this uses extra space but helps us understand how to merge two sorted arrays
+// // Brute: this uses extra space but helps us understand how to merge two sorted arrays
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void merge(long long arr1[], long long arr2[], int n, int m) {
+//     int left =0, right =0;
+//     int arr3[m+n], cur=0;
+//     while(left<n && right<m){
+//         if(arr1[left]<=arr2[right]){
+//             arr3[cur++] = arr1[left++];
+//         }
+//         else if(arr1[left]>arr2[right]){
+//             arr3[cur++] = arr2[right++];
+//         }
+//     }
+
+//     while (left < n) {
+//         arr3[cur++] = arr1[left++];
+//     }
+//     while (right < m) {
+//         arr3[cur++] = arr2[right++];
+//     }
+
+//     for (int i = 0; i < n + m; i++) {
+//         if (i < n) arr1[i] = arr3[i];
+//         else    arr2[i-n] = arr3[i];
+//     }
+
+// }
+
+// int main()
+// {
+//     long long arr1[] = {1, 4, 8, 10};
+//     long long arr2[] = {2, 3, 9};
+//     int n = 4, m = 3;
+//     merge(arr1, arr2, n, m);
+//     cout << "The merged arrays are: " << "\n";
+//     cout << "arr1[] = ";
+//     for (int i = 0; i < n; i++) {
+//         cout << arr1[i] << " ";
+//     }
+//     cout << "\narr2[] = ";
+//     for (int i = 0; i < m; i++) {
+//         cout << arr2[i] << " ";
+//     }
+//     cout << endl;
+//     return 0;
+// }
+
+// Optimal
+
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
 void merge(long long arr1[], long long arr2[], int n, int m) {
-    int left =0, right =0;
-    int arr3[m+n], cur=0;
-    while(left<n && right<m){
-        if(arr1[left]<=arr2[right]){
-            arr3[cur++] = arr1[left++];
+    int left = n-1, right = 0;
+    while(left>=0 && right<m){
+        if(arr1[left]>arr2[right]){
+            swap(arr1[left--], arr2[right++]);
         }
-        else if(arr1[left]>arr2[right]){
-            arr3[cur++] = arr2[right++];
+        else{
+            break;
         }
     }
-
-    while (left < n) {
-        arr3[cur++] = arr1[left++];
-    }
-    while (right < m) {
-        arr3[cur++] = arr2[right++];
-    }
-
-    for (int i = 0; i < n + m; i++) {
-        if (i < n) arr1[i] = arr3[i];
-        else    arr2[i-n] = arr3[i];
-    }
-
+    sort(arr1, arr1 + n);
+    sort(arr2, arr2 + m);
 }
 
 int main()
