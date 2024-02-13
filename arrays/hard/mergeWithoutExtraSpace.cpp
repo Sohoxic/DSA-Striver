5e477052aa0f7574429f9c94e6cc2e30ee95897d
Sohoxic <pes2202100825@pesu.pes.edu>
2024-02-13 07:56:17 +0530

    merge 2 array in sorted order

---
+++ arrays/hard/mergeWithoutExtraSpace.cpp
@@ -0,0 +1,48 @@
+// Brute: this uses extra space but helps us understand how to merge two sorted arrays
+#include <bits/stdc++.h>
+using namespace std;
+
+void merge(long long arr1[], long long arr2[], int n, int m) {
+    int left =0, right =0;
+    int arr3[m+n], cur=0;
+    while(left<n && right<m){
+        if(arr1[left]<=arr2[right]){
+            arr3[cur++] = arr1[left++];
+        }
+        else if(arr1[left]>arr2[right]){
+            arr3[cur++] = arr2[right++];
+        }
+    }
+
+    while (left < n) {
+        arr3[cur++] = arr1[left++];
+    }
+    while (right < m) {
+        arr3[cur++] = arr2[right++];
+    }
+
+    for (int i = 0; i < n + m; i++) {
+        if (i < n) arr1[i] = arr3[i];
+        else    arr2[i-n] = arr3[i];
+    }
+
+}
+
+int main()
+{
+    long long arr1[] = {1, 4, 8, 10};
+    long long arr2[] = {2, 3, 9};
+    int n = 4, m = 3;
+    merge(arr1, arr2, n, m);
+    cout << "The merged arrays are: " << "\n";
+    cout << "arr1[] = ";
+    for (int i = 0; i < n; i++) {
+        cout << arr1[i] << " ";
+    }
+    cout << "\narr2[] = ";
+    for (int i = 0; i < m; i++) {
+        cout << arr2[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
