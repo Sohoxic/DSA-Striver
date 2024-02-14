e930231565a562b16a41ea29c5cb6d6d6b8532ab
Sohoxic <pes2202100825@pesu.pes.edu>
2024-02-14 17:36:44 +0530

    added xor approach to finding repeating and missing number

--- arrays/hard/findRepeating&missing.cpp
+++ arrays/hard/findRepeating&missing.cpp
@@ -31,39 +31,94 @@
 //     return 0;
 // }
 
-// Mathematical Approach
+// // Mathematical Approach
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// vector<int> findMissingRepeatingNumbers(vector<int> a) {
+//     long long n = a.size(); // size of the array
+
+//     // Find Sn and S2n:
+//     long long SN = (n * (n + 1)) / 2;
+//     long long S2N = (n * (n + 1) * (2 * n + 1)) / 6;
+
+//     // Calculate S and S2:
+//     long long S = 0, S2 = 0;
+//     for (int i = 0; i < n; i++) {
+//         S += a[i];
+//         S2 += (long long)a[i] * (long long)a[i];
+//     }
+
+//     //S-Sn = X-Y:
+//     long long val1 = S - SN;
+
+//     // S2-S2n = X^2-Y^2:
+//     long long val2 = S2 - S2N;
+
+//     //Find X+Y = (X^2-Y^2)/(X-Y):
+//     val2 = val2 / val1;
+
+//     //Find X and Y: X = ((X+Y)+(X-Y))/2 and Y = X-(X-Y),
+//     // Here, X-Y = val1 and X+Y = val2:
+//     long long x = (val1 + val2) / 2;
+//     long long y = x - val1;
+
+//     return {(int)x, (int)y};
+// }
+
+// int main()
+// {
+//     vector<int> a = {3, 1, 2, 5, 4, 6, 7, 5};
+//     vector<int> ans = findMissingRepeatingNumbers(a);
+//     cout << "The repeating and missing numbers are: {"
+//          << ans[0] << ", " << ans[1] << "}\n";
+//     return 0;
+// }
+
+// xr approach
 #include <bits/stdc++.h>
 using namespace std;
 
 vector<int> findMissingRepeatingNumbers(vector<int> a) {
-    long long n = a.size(); // size of the array
-
-    // Find Sn and S2n:
-    long long SN = (n * (n + 1)) / 2;
-    long long S2N = (n * (n + 1) * (2 * n + 1)) / 6;
-
-    // Calculate S and S2:
-    long long S = 0, S2 = 0;
+    int n = a.size();
+    int xr = 0;
     for (int i = 0; i < n; i++) {
-        S += a[i];
-        S2 += (long long)a[i] * (long long)a[i];
+        xr = xr ^ a[i] ^ (i + 1);
     }
 
-    //S-Sn = X-Y:
-    long long val1 = S - SN;
+    int number = (xr & ~(xr - 1));
 
-    // S2-S2n = X^2-Y^2:
-    long long val2 = S2 - S2N;
+    int zero = 0;
+    int one = 0;
+    for (int i = 0; i < n; i++) {
+        //part of 1 group:
+        if ((a[i] & number) != 0) {
+            one = one ^ a[i];
+        }
+        //part of 0 group:
+        else {
+            zero = zero ^ a[i];
+        }
+    }
 
-    //Find X+Y = (X^2-Y^2)/(X-Y):
-    val2 = val2 / val1;
+    for (int i = 1; i <= n; i++) {
+        //part of 1 group:
+        if ((i & number) != 0) {
+            one = one ^ i;
+        }
+        //part of 0 group:
+        else {
+            zero = zero ^ i;
+        }
+    }
 
-    //Find X and Y: X = ((X+Y)+(X-Y))/2 and Y = X-(X-Y),
-    // Here, X-Y = val1 and X+Y = val2:
-    long long x = (val1 + val2) / 2;
-    long long y = x - val1;
+    int cnt = 0;
+    for (int i = 0; i < n; i++) {
+        if (a[i] == zero) cnt++;
+    }
 
-    return {(int)x, (int)y};
+    if (cnt == 2) return {zero, one};
+    return {one, zero};
 }
 
 int main()
@@ -74,3 +129,4 @@ int main()
          << ans[0] << ", " << ans[1] << "}\n";
     return 0;
 }
+
