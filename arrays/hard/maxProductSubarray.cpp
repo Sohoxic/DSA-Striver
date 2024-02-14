8ed61ec0973348c69445a2b7ab217e8cd7d0bb98
Sohoxic <pes2202100825@pesu.pes.edu>
2024-02-14 19:48:04 +0530

    added optimal max prod subarray

--- arrays/hard/maxProductSubarray.cpp
+++ arrays/hard/maxProductSubarray.cpp
@@ -1,20 +1,50 @@
-#include<bits/stdc++.h>
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int maxProductSubArray(vector<int>& nums) {
+//     int result = INT_MIN;
+//     for(int i=0;i<nums.size()-1;i++) {
+//         int prod = nums[i];
+//         for(int j=i+1;j<nums.size();j++) {
+//             prod *= nums[j];
+//             result = max(result,prod);    
+//         }
+//     }
+//     return result;
+// }
+
+// int main() {
+//     vector<int> nums = {1,2,-3,0,-4,-5};
+//     cout<<"The maximum product subarray: "<<maxProductSubArray(nums);
+//     return 0;
+// }
+
+//  Optimal
+
+
+
+#include <bits/stdc++.h>
 using namespace std;
 
-int maxProductSubArray(vector<int>& nums) {
-    int result = INT_MIN;
-    for(int i=0;i<nums.size()-1;i++) {
-        int prod = nums[i];
-        for(int j=i+1;j<nums.size();j++) {
-            prod *= nums[j];
-            result = max(result,prod);    
-        }
+int maxProductSubArray(vector<int> &arr) {
+    int n = arr.size(); //size of array.
+    int ans = INT_MIN;
+    int pre = 1, post = 1;
+    for (int i = 0; i < n; i++) {
+        if(pre==0)  pre = 1;
+        if(post==0) post = 1;
+
+        pre*= arr[i];
+        post*= arr[n-i-1];
+        ans = max(ans, max(pre, post));
     }
-    return result;
+    return ans;
 }
 
-int main() {
-    vector<int> nums = {1,2,-3,0,-4,-5};
-    cout<<"The maximum product subarray: "<<maxProductSubArray(nums);
+int main()
+{
+    vector<int> arr = {1, 2, -3, 0, -4, -5};
+    cout << "The maximum product subarray is: "
+         << maxProductSubArray(arr) << "\n";
     return 0;
 }
