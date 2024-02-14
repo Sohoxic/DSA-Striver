c56fd547aa591b1a95d72a6805741318f9376692
Sohoxic <pes2202100825@pesu.pes.edu>
2024-02-14 18:02:53 +0530

    added count inversion optimal code

--- arrays/hard/countInversion.cpp
+++ arrays/hard/countInversion.cpp
@@ -1,21 +1,92 @@
+// // Brute force
+// #include <bits/stdc++.h>
+// using namespace std;
 
+// int numberOfInversions(vector<int>&a, int n) {
+
+//     // Count the number of pairs:
+//     int cnt = 0;
+//     for(int i=0; i<n; i++){
+//         for(int j=i+1; j<n; j++){
+//             if(a[i]>a[j]){
+//                 cnt++;
+//             }
+//         }
+//     }
+//     return cnt;
+// }
+
+// int main()
+// {
+//     vector<int> a = {5, 4, 3, 2, 1};
+//     int n = 5;
+//     int cnt = numberOfInversions(a, n);
+//     cout << "The number of inversions is: "
+//          << cnt << endl;
+//     return 0;
+// }
 
 
 #include <bits/stdc++.h>
 using namespace std;
 
+int merge(vector<int> &arr, int low, int mid, int high) {
+    vector<int> temp; // temporary array
+    int left = low;      // starting index of left half of arr
+    int right = mid + 1;   // starting index of right half of arr
+
+    //Modification 1: cnt variable to count the pairs:
+    int cnt = 0;
+
+    //storing elements in the temporary array in a sorted manner//
+
+    while (left <= mid && right <= high) {
+        if (arr[left] <= arr[right]) {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else {
+            temp.push_back(arr[right]);
+            cnt += (mid - left + 1); //Modification 2
+            right++;
+        }
+    }
+
+    // if elements on the left half are still left //
+
+    while (left <= mid) {
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    //  if elements on the right half are still left //
+    while (right <= high) {
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    // transfering all elements from temporary to arr //
+    for (int i = low; i <= high; i++) {
+        arr[i] = temp[i - low];
+    }
+
+    return cnt; // Modification 3
+}
+
+int mergeSort(vector<int> &arr, int low, int high) {
+    int cnt = 0;
+    int mid = (low+high)/2;
+    if(low>=high)   return cnt;
+    cnt+= mergeSort(arr, low, mid);
+    cnt+= mergeSort(arr, mid+1, high);
+    cnt+= merge(arr, low, mid, high);
+    return cnt;
+}
+
 int numberOfInversions(vector<int>&a, int n) {
 
     // Count the number of pairs:
-    int cnt = 0;
-    for(int i=0; i<n; i++){
-        for(int j=i+1; j<n; j++){
-            if(a[i]>a[j]){
-                cnt++;
-            }
-        }
-    }
-    return cnt;
+    return mergeSort(a, 0, n - 1);
 }
 
 int main()
@@ -23,7 +94,8 @@ int main()
     vector<int> a = {5, 4, 3, 2, 1};
     int n = 5;
     int cnt = numberOfInversions(a, n);
-    cout << "The number of inversions is: "
+    cout << "The number of inversions are: "
          << cnt << endl;
     return 0;
 }
+
