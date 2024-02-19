8bc02cbe2b2cc65d1d932c300021bbdf9313fc10
Sohoxic <pes2202100825@pesu.pes.edu>
2024-02-19 07:17:10 +0530

    added using morris traversal to flatten a binary tree

--- binary trees/types to remember/flatternBinaryTree.cpp
+++ binary trees/types to remember/flatternBinaryTree.cpp
@@ -1,5 +1,62 @@
-// Binary tree to linked list -> preorder
+// // Binary tree to linked list -> preorder
 
+// #include <bits/stdc++.h>
+
+// using namespace std;
+
+// struct node {
+//   int data;
+//   struct node * left, * right;
+// };
+
+// class Solution {
+//   node * prev = NULL;
+//   public:
+//     void flatten(node * root) {
+//       if (root == NULL) return;
+
+//       flatten(root -> right);
+//       flatten(root -> left);
+
+//       root -> right = prev;
+//       root -> left = NULL;
+//       prev = root;
+//     }
+
+// };
+
+// struct node * newNode(int data) {
+//   struct node * node = (struct node * ) malloc(sizeof(struct node));
+//   node -> data = data;
+//   node -> left = NULL;
+//   node -> right = NULL;
+
+//   return (node);
+// }
+
+// int main() {
+
+//   struct node * root = newNode(1);
+//   root -> left = newNode(2);
+//   root -> left -> left = newNode(3);
+//   root -> left -> right = newNode(4);
+//   root -> right = newNode(5);
+//   root -> right -> right = newNode(6);
+//   root -> right -> right -> left = newNode(7);
+
+//   Solution obj;
+
+//   obj.flatten(root);
+//   while(root->right!=NULL)
+//   {
+//       cout<<root->data<<"->";
+//       root=root->right;
+//   }
+// cout<<root->data;
+//   return 0;
+// }
+
+// using morris traversal
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -10,17 +67,20 @@ struct node {
 };
 
 class Solution {
-  node * prev = NULL;
   public:
-    void flatten(node * root) {
-      if (root == NULL) return;
-
-      flatten(root -> right);
-      flatten(root -> left);
-
-      root -> right = prev;
-      root -> left = NULL;
-      prev = root;
+   void flatten(node* root) {
+        node* cur = root;
+        while(cur){
+          if(cur->left){
+            node *prev = cur->left;
+            while(prev->right){
+              prev = prev->right;
+            }
+            prev->right = cur->right;
+            cur->right = cur->left;
+          }
+          cur = cur->right;
+        }
     }
 
 };
