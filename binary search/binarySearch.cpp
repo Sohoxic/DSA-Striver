df94043e033b4de8b1f3ab348abe6dc9b8e94c00
Sohoxic <pes2202100825@pesu.pes.edu>
2024-02-20 07:37:19 +0530

    added binary search recursive approach

--- binary search/binarySearch.cpp
+++ binary search/binarySearch.cpp
@@ -1,23 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 // Iterative
-int binarySearch(vector<int>& nums, int n, int target) {
-    int low = 0;
-    int high = n-1;
+// int binarySearch(vector<int>& nums, int n, int target) {
+//     int low = 0;
+//     int high = n-1;
     
-    while(low<=high){
+//     while(low<=high){
+//         int mid = (low+high)/2;
+//         if(nums[mid]==target){
+//             return mid;
+//         }
+//         else if(target<nums[mid]){
+//             high = mid - 1;
+//         }
+//         else{
+//             low = low+1;
+//         }
+//     }
+//     return -1;
+// }
+
+// Recursive
+int search(vector<int>& nums, int n, int low, int high, int target) {
         int mid = (low+high)/2;
+        if(low>high) return -1;
         if(nums[mid]==target){
             return mid;
         }
         else if(target<nums[mid]){
-            high = mid - 1;
+            return search(nums, n, low, mid - 1, target);
         }
         else{
-            low = low+1;
+            return search(nums, n, mid+1, high, target);
         }
-    }
-    return -1;
+}
+
+int binarySearch(vector<int> nums,int target){
+    int n = nums.size();
+    int low = 0;
+    int high = n-1;
+    return search(nums, n, low, high, target);
 }
 
 int main()
@@ -25,7 +47,7 @@ int main()
     vector<int> a = {3, 4, 6, 7, 9, 12, 16, 17};
     int target = 6;
     int n = a.size();
-    int ind = binarySearch(a, n, target);
+    int ind = binarySearch(a, target);
     if (ind == -1) cout << "The target is not present." << endl;
     else cout << "The target is at index: "
                   << ind << endl;
