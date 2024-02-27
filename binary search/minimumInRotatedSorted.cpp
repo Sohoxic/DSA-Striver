92090e63df221c618237d57dcadc59968eef20be
Sohoxic <sarkarsoham73@gmail.com>
2024-02-27 19:54:44 +0530

    added minimum in rotated array

--- binary search/minimumInRotatedSorted.cpp
+++ binary search/minimumInRotatedSorted.cpp
@@ -7,22 +7,13 @@ int findMin(vector<int>& arr) {
     int ans = INT_MAX;
     while (low <= high) {
         int mid = (low + high) / 2;
-
-        //if left part is sorted:
-        if (arr[low] <= arr[mid]) {
-            // keep the minimum:
+        // left sorted
+        if(arr[low]<=arr[mid]){
             ans = min(ans, arr[low]);
-
-            // Eliminate left half:
             low = mid + 1;
         }
-        else { //if right part is sorted:
-
-            // keep the minimum:
+        else{
             ans = min(ans, arr[mid]);
-
-            // Eliminate right half:
-            high = mid - 1;
         }
     }
     return ans;
