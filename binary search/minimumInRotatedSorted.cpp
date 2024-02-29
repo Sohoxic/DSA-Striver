dc081eb4a01f22bc2226107f8441c4dc5589a516
Sohoxic <sarkarsoham73@gmail.com>
2024-02-29 21:14:03 +0530

    added: optimised minimum in rotated sorted array

--- binary search/minimumInRotatedSorted.cpp
+++ binary search/minimumInRotatedSorted.cpp
@@ -1,3 +1,36 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int findMin(vector<int>& arr) {
+//     int low = 0, high = arr.size() - 1;
+//     int ans = INT_MAX;
+//     while (low <= high) {
+//         int mid = (low + high) / 2;
+//         // left sorted
+//         if(arr[low]<=arr[mid]){
+//             ans = min(ans, arr[low]);
+//             low = mid + 1;
+//         }
+//         // right sorted
+//         else{
+//             ans = min(ans, arr[mid]);
+//             high = mid - 1;
+//         }
+//     }
+//     return ans;
+// }
+
+// int main()
+// {
+//     vector<int> arr = {4, 5, 6, 7, 0, 1, 2, 3};
+//     int ans = findMin(arr);
+//     cout << "The minimum element is: " << ans << "\n";
+//     return 0;
+// }
+
+
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -6,14 +39,28 @@ int findMin(vector<int>& arr) {
     int ans = INT_MAX;
     while (low <= high) {
         int mid = (low + high) / 2;
-        // left sorted
-        if(arr[low]<=arr[mid]){
+        //search space is already sorted
+        //then arr[low] will always be
+        //the minimum in that search space:
+        if (arr[low] <= arr[high]) {
             ans = min(ans, arr[low]);
+            break;
+        }
+
+        //if left part is sorted:
+        if (arr[low] <= arr[mid]) {
+            // keep the minimum:
+            ans = min(ans, arr[low]);
+
+            // Eliminate left half:
             low = mid + 1;
         }
-        // right sorted
-        else{
+        else { //if right part is sorted:
+
+            // keep the minimum:
             ans = min(ans, arr[mid]);
+
+            // Eliminate right half:
             high = mid - 1;
         }
     }
