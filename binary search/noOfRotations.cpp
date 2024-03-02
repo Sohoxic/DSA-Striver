8dc3b0d3cdb894337eaaa8fb8fb5061442bf2345
Sohoxic <sarkarsoham73@gmail.com>
2024-03-02 11:33:49 +0530

    added: no. of rotations in a sorted array, modified a condition in minimum rotated array

--- binary search/noOfRotations.cpp
+++ binary search/noOfRotations.cpp
@@ -4,32 +4,40 @@ using namespace std;
 int findMin(vector<int>& arr) {
     int low = 0, high = arr.size() - 1;
     int ans = INT_MAX;
+    int index = 0;
     while (low <= high) {
         int mid = (low + high) / 2;
         // both side sorted, that is entire array is sorted
         if(arr[low]<=arr[high]){
+            index = low;
             ans = min(ans, arr[low]);
             break;
         }
         // left sorted
         if(arr[low]<=arr[mid]){
-            ans = min(ans, arr[low]);
+            if(arr[low]<ans){
+                index = low;
+                ans = arr[low];
+            }
             // cout<<ans;
             low = mid + 1;
         }
         else{
-            ans = min(ans, arr[mid]);
+           if(arr[mid]<ans){
+                index = mid;
+                ans = arr[low];
+            }
             // cout<<ans;
             high = mid - 1;
         }
     }
-    return ans;
+    return index;
 }
 
 int main()
 {
     vector<int> arr = {4, 5, 6, 7, 0, 1, 2, 3};
     int ans = findMin(arr);
-    cout << "The minimum element is: " << ans << "\n";
+    cout << "The number of rotation is: " << ans << "\n";
     return 0;
 }
