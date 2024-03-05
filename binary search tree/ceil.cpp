9fc47d2e2b80220b4abce39eea8e28f43898526c
Sohoxic <sarkarsoham73@gmail.com>
2024-03-05 14:36:55 +0530

    added: find ceil of a given number

---
+++ binary search tree/ceil.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//Declaring the struct for Tree implementation.
+struct node
+{
+    int data;
+    struct node *left, *right;
+};
+
+//Defining a function for inserting new-node in BST
+struct node *newNode(int data)
+{
+    struct node *node = (struct node *)malloc(sizeof(struct node));
+    node->data = data;
+    node->left = NULL;
+    node->right = NULL;
+
+    return (node);
+}
+
+int findCeil(node *root, int input)
+{
+    int ceil = -1;
+    while(curr && input<curr->data){
+        if(root->data == input){
+            return root->data;
+        }
+        else if(input<root->data){
+            ceil = root->data;
+            root = root->left;
+        }
+        root = root->right;
+    }
+
+    return ceil;
+}
+
+int main()
+{
+    // Driver Code.
+    struct node *root = newNode(10);
+    root->left = newNode(5);
+    root->left->left = newNode(4);
+    root->left->right = newNode(7);
+    root->left->right->right = newNode(8);
+    root->right = newNode(11);
+
+    cout << findCeil(root, 6);
+    cout << endl;
+
+    return 0;
+}
